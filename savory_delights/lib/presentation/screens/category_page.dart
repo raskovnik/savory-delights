bc1eff7eb0dadfddc4360bcbdf8782d6e161cc11
raskovnik <4lch3my.3ck0@gmail.com>
2023-07-07 19:11:37 +0300

    change category page to use gridview

--- savory_delights/lib/presentation/screens/category_page.dart
+++ savory_delights/lib/presentation/screens/category_page.dart
@@ -9,30 +9,36 @@ class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Center(child: Text("Categories")), backgroundColor: Colors.blueAccent),
+        appBar: AppBar(
+            title: const Center(child: Text("Categories")),
+            backgroundColor: Colors.blueAccent),
         body: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               BlocBuilder<RecipeCubit, RecipeState>(
-                  builder: (context, state) {
-                    if (state is RecipeLoading) {
-                      return const Center(child: CircularProgressIndicator());
-                    } else if (state is RecipeLoaded) {
-                      return Expanded(
-                        child: ListView.builder(
+                builder: (context, state) {
+                  if (state is RecipeLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is RecipeLoaded) {
+                    return Expanded(
+                      child: GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, // Number of columns in the grid
+                            crossAxisSpacing: 10, // Spacing between columns
+                            mainAxisSpacing: 10, // Spacing between rows
+                          ),
                           itemCount: state.categories.length,
                           itemBuilder: (context, index) {
-                            return CategoryWidget(category: state.categories[index]);
-                          }
-                        ),
-                      );
-                    }                
-                    return Container();
-                  },
-                ),
-            ]
-        )
-      );
+                            return CategoryWidget(
+                                category: state.categories[index]);
+                          }),
+                    );
+                  }
+                  return Container();
+                },
+              ),
+            ]));
   }
 }
--- savory_delights/lib/presentation/widgets/recipe_card.dart
+++ savory_delights/lib/presentation/widgets/recipe_card.dart
@@ -3,29 +3,61 @@ import 'package:the_mealdb_api/the_mealdb_api.dart';
 
 class RecipeCard extends StatelessWidget {
   final Recipe recipe;
-  const RecipeCard({required this.recipe, super.key});
+
+  const RecipeCard({required this.recipe, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.25,
-      width: MediaQuery.of(context).size.width * .85,
+      width: MediaQuery.of(context).size.width * 0.85,
       child: GestureDetector(
         onTap: () {
           print("Card has been clicked");
         },
         child: Card(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(21.0),
-                image: DecorationImage(
-                  image: NetworkImage(recipe.imageURL),
-                  fit: BoxFit.cover
-                )
-              )
-            ),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(21.0),
+                  image: DecorationImage(
+                    image: NetworkImage(recipe.imageURL),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 5,
+                left: 10,
+                child: Container(
+                  color: Colors.black.withOpacity(0.25),
+                  child: Text(
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    recipe.title,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const Positioned(
+                top: 10,
+                right: 10,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                  size: 24,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
