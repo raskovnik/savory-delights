46d4f730bbf7e474a2267a6e84d5d75b0187bcd0
raskovnik <4lch3my.3ck0@gmail.com>
2023-07-09 10:41:57 +0300

    initial recipe page design

--- savory_delights/lib/presentation/screens/recipe_page.dart
+++ savory_delights/lib/presentation/screens/recipe_page.dart
@@ -8,23 +8,46 @@ class RecipePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => RecipeCubit(id: id),
-      child: Scaffold(body: BlocBuilder<RecipeCubit, RecipeState>(
-        builder: (context, state) {
-          if (state is RecipeLoading) {
-            return const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [Center(child: CircularProgressIndicator()), Center(child: Text("Hold On Chef!"))]);
-          } else if (state is RecipeLoaded) {
-            return Center(child: Text(state.data.instructions,));
-          } else if (state is RecipeLoadFailed) {
-            return Center(child: Text(state.e));
-          }
-          return Container();
-        },
-      )),
+    return DefaultTabController(
+      length: 2,
+      child: BlocProvider(
+        create: (context) => RecipeCubit(id: id),
+        child: Scaffold(
+          appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Ingredients"),
+              Tab(text: "Instructions")
+            ]
+          ),
+          ),
+          body: BlocBuilder<RecipeCubit, RecipeState>(
+          builder: (context, state) {
+            if (state is RecipeLoading) {
+              return const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [Center(child: CircularProgressIndicator()), Center(child: Text("Hold On Chef!"))]);
+            } else if (state is RecipeLoaded) {
+              return TabBarView(
+                children: [
+                  ListView.builder(
+                    itemCount: state.data.ingredients.length,
+                    itemBuilder: (context, index) {
+                      return Text("${state.data.ingredients[index]} - ${state.data.measures[index]}");
+                    },
+                  ),
+                  SingleChildScrollView(child: Text(state.data.instructions))
+                ]
+              );
+              // return Center(child: Text(state.data.instructions));
+            } else if (state is RecipeLoadFailed) {
+              return Center(child: Text(state.e, style: const TextStyle(wordSpacing: 2.0, height: 5.0)));
+            }
+            return Container();
+          },
+        )),
+      ),
     );
   }
 }
